2f009d5864a88e0874d1e909e6c9ac1f2d6f6202
Luís Câmara <luis.camara@live.com.pt>
2014-10-18 08:31:00 +0100

    Create MainActivity.java

---
+++ com/luismark/android/SkyWatcher/MainActivity.java
@@ -0,0 +1,19 @@
+// MainActivity.java
+// The main file for building SkyWatcher for Android.
+
+package com.luismark.android.SkyWatcher;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+import java.util.*;
+
+public class MainActivity extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        
+        // TODO: Insert code here
+    }
+}
